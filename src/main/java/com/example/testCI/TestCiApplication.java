e61e6beade823cb0d46db6b4f5ea958fdb4a0853
SEHAN MADUSHANKA <sehanmadushanka0714@gmail.com>
2025-03-29 22:01:46 +0530

    Initial commit

---
+++ src/main/java/com/example/testCI/TestCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.testCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCiApplication.class, args);
+	}
+
+}
